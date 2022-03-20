b63319046a633a252f1d97b081c7a65006b6c60a
brGuirra <br.guirra@gmail.com>
2022-03-20 19:40:16 -0300

    add login view

---
+++ bankey/bankey/Login/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  bankey
+//
+//  Created by Bruno Guirra on 20/03/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    let loginView = LoginView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        style()
+        layout()
+    }
+}
+
+extension LoginViewController {
+    
+    private func style() {
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func layout() {
+        view.addSubview(loginView)
+        
+        NSLayoutConstraint.activate([
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
+        ])
+    }
+}
+
