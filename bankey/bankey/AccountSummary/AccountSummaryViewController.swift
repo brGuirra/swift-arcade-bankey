9ab1a12e674c42f1b9ba16904aab4dd033e2ae84
brGuirra <br.guirra@gmail.com>
2022-04-11 19:25:24 -0300

    feat: add alerts to display when erros fetching and parsing data occur

--- bankey/bankey/AccountSummary/AccountSummaryViewController.swift
+++ bankey/bankey/AccountSummary/AccountSummaryViewController.swift
@@ -175,7 +175,19 @@ extension AccountSummaryViewController {
                 case .success(let profile):
                     self.profile = profile
                 case .failure(let error):
-                    print(error.localizedDescription)
+                    let title: String
+                    let message: String
+                    
+                    switch error {
+                        case .serverError:
+                            title = "Server Error"
+                            message = "Ensure you are connected to the internet. Please try again."
+                        case .decodingError:
+                            title = "Decoding Error"
+                            message = "We could not process your request. Please try again."
+                    }
+                    
+                    self.showErrorAlert(title: title, message: message)
             }
             group.leave()
         }
@@ -221,5 +233,15 @@ extension AccountSummaryViewController {
                                          accountName: acc.name,
                                          balance: acc.amount)
         }
+    }
+    
+    private func showErrorAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
     }
 }
