dde6aaeb68a246d5d22874d4067414e80ea6afe5
brGuirra <br.guirra@gmail.com>
2022-04-11 19:32:06 -0300

    refactor: add a separate function to display errors

--- bankey/bankey/AccountSummary/AccountSummaryViewController.swift
+++ bankey/bankey/AccountSummary/AccountSummaryViewController.swift
@@ -175,19 +175,7 @@ extension AccountSummaryViewController {
                 case .success(let profile):
                     self.profile = profile
                 case .failure(let error):
-                    let title: String
-                    let message: String
-                    
-                    switch error {
-                        case .serverError:
-                            title = "Server Error"
-                            message = "Ensure you are connected to the internet. Please try again."
-                        case .decodingError:
-                            title = "Decoding Error"
-                            message = "We could not process your request. Please try again."
-                    }
-                    
-                    self.showErrorAlert(title: title, message: message)
+                    self.displayError(error)
             }
             group.leave()
         }
@@ -200,7 +188,7 @@ extension AccountSummaryViewController {
                 case .success(let accounts):
                     self.accounts = accounts
                 case .failure(let error):
-                    print(error.localizedDescription)
+                    self.displayError(error)
             }
             group.leave()
         }
@@ -233,6 +221,22 @@ extension AccountSummaryViewController {
                                          accountName: acc.name,
                                          balance: acc.amount)
         }
+    }
+    
+    private func displayError(_ error: NetworkError) {
+        let title: String
+        let message: String
+        
+        switch error {
+            case .serverError:
+                title = "Server Error"
+                message = "Ensure you are connected to the internet. Please try again."
+            case .decodingError:
+                title = "Decoding Error"
+                message = "We could not process your request. Please try again."
+        }
+        
+        showErrorAlert(title: title, message: message)
     }
     
     private func showErrorAlert(title: String, message: String) {
